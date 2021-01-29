8391dc1349a2ffff71a4a9e7154bcd520c1426e2
Michaela University Ununtu <michaela.sahlgren@gmail.com>
2021-01-29 18:16:59 +0100

    add: Program skeleton #21

---
+++ src/main/java/Geometry.java
@@ -0,0 +1,6 @@
+public class Geometry{
+  public double calculateDistance(double x1, double y1, double x2, double y2){
+    //issue#1
+    return 0;
+  }
+}
---
+++ src/main/java/PreliminaryUnlockingMatrix.java
@@ -0,0 +1,9 @@
+public class PreliminaryUnlockingMatrix {
+  public void createPum(
+    boolean[] cmv,
+    LogicalConnectorMatrix lcm,
+    boolean[][] pum //createPum populates the values in pum
+    ) {
+      //issue#19
+  }
+}
