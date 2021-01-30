5cb6fa259a7204db05d63f9b371803a64e020dab
Michaela University Ununtu <michaela.sahlgren@gmail.com>
2021-01-30 17:00:14 +0100

    fix: yCoordinates are arrays #21
    Change the yCoordinates parameters to all be double arrays, in the methods in ConditionsMetVector.

--- src/main/java/ConditionsMetVector.java
+++ src/main/java/ConditionsMetVector.java
@@ -1,75 +1,75 @@
 public class ConditionsMetVector {
-  public boolean calculateRule0(double[] xCoordinates, double yCoordinates, double LENGTH1) {
+  public boolean calculateRule0(double[] xCoordinates, double[] yCoordinates, double LENGTH1) {
     //issue#2
     return false;
   }
 
-  public boolean calculateRule1(double[] xCoordinates, double yCoordinates, double RADIUS1) {
+  public boolean calculateRule1(double[] xCoordinates, double[] yCoordinates, double RADIUS1) {
     //issue#3
     return false;
   }
 
-  public boolean calculateRule2(double[] xCoordinates, double yCoordinates, double EPSILON) {
+  public boolean calculateRule2(double[] xCoordinates, double[] yCoordinates, double EPSILON) {
     //issue#4
     return false;
   }
 
-  public boolean calculateRule3(double[] xCoordinates, double yCoordinates, double AREA1) {
+  public boolean calculateRule3(double[] xCoordinates, double[] yCoordinates, double AREA1) {
     //issue#5
     return false;
   }
 
-  public boolean calculateRule4(double[] xCoordinates, double yCoordinates, int Q_PTS) {
+  public boolean calculateRule4(double[] xCoordinates, double[] yCoordinates, int Q_PTS) {
     //issue#6
     return false;
   }
 
-  public boolean calculateRule5(double[] xCoordinates, double yCoordinates) {
+  public boolean calculateRule5(double[] xCoordinates, double[] yCoordinates) {
     //issue#7
     return false;
   }
 
-  public boolean calculateRule6(double[] xCoordinates, double yCoordinates, int N_PTS, double DIST) {
+  public boolean calculateRule6(double[] xCoordinates, double[] yCoordinates, int N_PTS, double DIST) {
     //issue#8
     return false;
   }
 
-  public boolean calculateRule7(double[] xCoordinates, double yCoordinates, int K_PTS, double LENGTH1) {
+  public boolean calculateRule7(double[] xCoordinates, double[] yCoordinates, int K_PTS, double LENGTH1) {
     //issue#9
     return false;
   }
 
-  public boolean calculateRule8(double[] xCoordinates, double yCoordinates, int A_PTS, int B_PTS, double RADIUS1) {
+  public boolean calculateRule8(double[] xCoordinates, double[] yCoordinates, int A_PTS, int B_PTS, double RADIUS1) {
     //issue#10
     return false;
   }
 
-  public boolean calculateRule9(double[] xCoordinates, double yCoordinates, int C_PTS, int D_PTS, double EPSILON) {
+  public boolean calculateRule9(double[] xCoordinates, double[] yCoordinates, int C_PTS, int D_PTS, double EPSILON) {
     //issue#11
     return false;
   }
 
-  public boolean calculateRule10(double[] xCoordinates, double yCoordinates, int E_PTS, int F_PTS, double AREA1) {
+  public boolean calculateRule10(double[] xCoordinates, double[] yCoordinates, int E_PTS, int F_PTS, double AREA1) {
     //issue#12
     return false;
   }
 
-  public boolean calculateRule11(double[] xCoordinates, double yCoordinates, int G_PTS) {
+  public boolean calculateRule11(double[] xCoordinates, double[] yCoordinates, int G_PTS) {
     //issue#13
     return false;
   }
 
-  public boolean calculateRule12(double[] xCoordinates, double yCoordinates, int K_PTS, double LENGTH1, double LENGTH2) {
+  public boolean calculateRule12(double[] xCoordinates, double[] yCoordinates, int K_PTS, double LENGTH1, double LENGTH2) {
     //issue#14
     return false;
   }
 
-  public boolean calculateRule13(double[] xCoordinates, double yCoordinates, int A_PTS, int B_PTS, double RADIUS1, double RADIUS2) {
+  public boolean calculateRule13(double[] xCoordinates, double[] yCoordinates, int A_PTS, int B_PTS, double RADIUS1, double RADIUS2) {
     //issue#15
     return false;
   }
 
-  public boolean calculateRule14(double[] xCoordinates, double yCoordinates, int E_PTS, int F_PTS, double AREA1, double AREA2) {
+  public boolean calculateRule14(double[] xCoordinates, double[] yCoordinates, int E_PTS, int F_PTS, double AREA1, double AREA2) {
     //issue#16
     return false;
   }
