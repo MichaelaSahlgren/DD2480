4ee7c130ac2116973d11ef6441e04bcf343398f4
George Bassilious <35694658+George-Bassilious@users.noreply.github.com>
2021-01-31 12:00:21 +0100

    add: Test class for distance calculation #1
    added test cases for the calculate distance method, one valid and one invalid.

--- src/test/java/GeometryTests.java
+++ src/test/java/GeometryTests.java
@@ -1,45 +1,39 @@
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
-class DecideTests {
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+class GeometryTests {
+    private Geometry geometryHelper;
+    @BeforeEach
+    public void setUp() throws Exception {
+        geometryHelper = new Geometry();
+    }
+
 	@Test
-	@DisplayName("PUV all false")
-	void puvAllFalse() {
-    // If every element in the PUV is false, then every element in the FUV
-    // should end up being true, so that the launch decision should be
-    // true regardless of all other inputs.
-		Decide decider = new Decide();
-    double[] xCoordinates = {1, 1, 1};
-    double[] yCoordinates = {2, 2, 2};
-    Parameters parameters = new Parameters();
-    parameters.LENGTH1 = 0;
-    parameters.RADIUS1 = 0;
-    parameters.EPSILON = 0;
-    parameters.AREA1 = 0;
-    parameters.Q_PTS = 0;
-    parameters.QUADS = 0;
-    parameters.DIST = 0;
-    parameters.N_PTS = 0;
-    parameters.K_PTS = 0;
-    parameters.A_PTS = 0;
-    parameters.B_PTS = 0;
-    parameters.C_PTS = 0;
-    parameters.D_PTS = 0;
-    parameters.E_PTS = 0;
-    parameters.F_PTS = 0;
-    parameters.G_PTS = 0;
-    parameters.LENGTH2 = 0;
-    parameters.RADIUS2 = 0;
-    parameters.AREA2 = 0;
-    LogicalConnectorMatrix lcm = new LogicalConnectorMatrix();
-    // Set every element of the input PUV to false:
-    boolean[] puv = {false, false, false, false, false, false, false, false, false, false, false, false, false, false, false};
-    boolean[] cmv = new boolean[15];
-    boolean[][] pum = new boolean[15][15];
-    boolean[] fuv = new boolean[15];
-    boolean launch = decider.decide(xCoordinates, yCoordinates, parameters, lcm, puv, cmv, pum, fuv);
-		assertTrue(launch);
-	}
+	@DisplayName("Calculate Distance valid")
+	void calculateDistanceValid() {
+
+        double[] xCoordinates = {15, 35};
+        double[] yCoordinates = {20, 5};
+        double expectedLength = 25;
+        double distanceBetweenOneAndTwo= geometryHelper.calculateDistance(xCoordinates[0],yCoordinates[0],xCoordinates[1],yCoordinates[1]);
+        assertEquals(expectedLength,distanceBetweenOneAndTwo ,"Distance calculated correct");
+    }
+
+    @Test
+    @DisplayName("Calculate Distance invalid")
+    void calculateDistanceInvalid() {
+
+        double[] xCoordinates = {3, 4};
+        double[] yCoordinates = {0, 0};
+        double notExpectedLength = 5;
+        double distanceBetweenOneAndTwo= geometryHelper.calculateDistance(xCoordinates[0],yCoordinates[0],xCoordinates[1],yCoordinates[1]);
+        assertNotEquals(notExpectedLength,distanceBetweenOneAndTwo ,"Distance calculated correct");
+
+    }
 }
