de04db1958dc07efdb7799d36b489d967d76bc5d
George Bassilious <35694658+George-Bassilious@users.noreply.github.com>
2021-01-31 18:04:15 +0100

    add: testClass for CMV class with test cases for LIC2 #4
    added test method to verify LIC2 calculations

--- src/test/java/ConditionsMetVectorTest.java
+++ src/test/java/ConditionsMetVectorTest.java
@@ -1,45 +1,59 @@
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-class DecideTests {
-	@Test
-	@DisplayName("PUV all false")
-	void puvAllFalse() {
-    // If every element in the PUV is false, then every element in the FUV
-    // should end up being true, so that the launch decision should be
-    // true regardless of all other inputs.
-		Decide decider = new Decide();
-    double[] xCoordinates = {1, 1, 1};
-    double[] yCoordinates = {2, 2, 2};
-    Parameters parameters = new Parameters();
-    parameters.LENGTH1 = 0;
-    parameters.RADIUS1 = 0;
-    parameters.EPSILON = 0;
-    parameters.AREA1 = 0;
-    parameters.Q_PTS = 0;
-    parameters.QUADS = 0;
-    parameters.DIST = 0;
-    parameters.N_PTS = 0;
-    parameters.K_PTS = 0;
-    parameters.A_PTS = 0;
-    parameters.B_PTS = 0;
-    parameters.C_PTS = 0;
-    parameters.D_PTS = 0;
-    parameters.E_PTS = 0;
-    parameters.F_PTS = 0;
-    parameters.G_PTS = 0;
-    parameters.LENGTH2 = 0;
-    parameters.RADIUS2 = 0;
-    parameters.AREA2 = 0;
-    LogicalConnectorMatrix lcm = new LogicalConnectorMatrix();
-    // Set every element of the input PUV to false:
-    boolean[] puv = {false, false, false, false, false, false, false, false, false, false, false, false, false, false, false};
-    boolean[] cmv = new boolean[15];
-    boolean[][] pum = new boolean[15][15];
-    boolean[] fuv = new boolean[15];
-    boolean launch = decider.decide(xCoordinates, yCoordinates, parameters, lcm, puv, cmv, pum, fuv);
-		assertTrue(launch);
-	}
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class ConditionsMetVectorTest {
+    ConditionsMetVector controller;
+    Parameters parameters;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        controller = new ConditionsMetVector();
+        parameters = new Parameters();
+    }
+
+    @Test
+    @DisplayName("LIC #2 tests")
+    void licTwoTestValid() {
+
+        parameters.EPSILON = 1;
+
+        //true Test cases
+        //check if the LIC2 condition is met
+        double[] xCoords = {1, -1, 0, -1, 1};
+        double[] yCoords = {2, -2, 0, -2, 2};
+
+        Boolean trueTest = controller.calculateRule2(xCoords, yCoords, parameters.EPSILON);
+        assertTrue(trueTest);
+
+        //if the points coincide, they should be ignored but not break the function
+        xCoords = new double[]{1, 1, -1, 0, -1, 1};
+        yCoords = new double[]{2, 2, -2, 0, -2, 2};
+        trueTest = controller.calculateRule2(xCoords, yCoords, parameters.EPSILON);
+        assertTrue(trueTest);
+
+        //false TEST cases
+        //if there are less than 3 points
+        xCoords = new double[]{1, -1};
+        yCoords = new double[]{1, -2};
+        Boolean falseTest = controller.calculateRule2(xCoords, yCoords, parameters.EPSILON);
+        assertFalse(falseTest);
+
+        //if the points coincide, they should be ignored
+        xCoords = new double[]{1, 1, -3};
+        yCoords = new double[]{1, 1, -2.5};
+        falseTest = controller.calculateRule2(xCoords, yCoords, parameters.EPSILON);
+        assertFalse(falseTest);
+
+        //check if the LIC2 condition is not met
+        xCoords = new double[]{0, 3, 6};
+        yCoords = new double[]{0, -1, 0};
+        falseTest = controller.calculateRule2(xCoords, yCoords, parameters.EPSILON);
+        assertFalse(falseTest);
+
+    }
+
 }
