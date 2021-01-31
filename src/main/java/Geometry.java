c72e60ef5d91da0ae31e9e72e04717c4f06ce22c
George Bassilious <35694658+George-Bassilious@users.noreply.github.com>
2021-01-31 11:57:10 +0100

    fix: fixed minor bug #1
    fixed minor bug in calculating distance method

--- src/main/java/Geometry.java
+++ src/main/java/Geometry.java
@@ -2,6 +2,7 @@ public class Geometry{
   public double calculateDistance(double x1, double y1, double x2, double y2){
     //issue#1
     //Solved using pythagoras theorem
-    return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
+    return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
   }
+
 }
