1907cd1f2729f7cf8daf1f28138517cd7ce6cb7f
George Bassilious <35694658+George-Bassilious@users.noreply.github.com>
2021-02-01 09:46:29 +0100

    Refactor: improve LIC2 method #4
    variables used in the for-loop are now declared out of the loop

--- src/main/java/ConditionsMetVector.java
+++ src/main/java/ConditionsMetVector.java
@@ -11,7 +11,20 @@ public class ConditionsMetVector {
 
     public boolean calculateRule2(double[] xCoordinates, double[] yCoordinates, double EPSILON) {
         //issue#4
+        
+        //point 1
+        double p1x = 0;
+        double p2x = 0;
+        
+        //point 2
+        double p2x = 0;
+        double p2y = 0;
+        
+        //point 3
+        double p3x = 0;
+        double p3y = 0;
 
+        double angle = 0;
 
         if (xCoordinates.length < 3 || yCoordinates.length < 3) {
             return false;
@@ -19,22 +32,24 @@ public class ConditionsMetVector {
 
         for (int i = 0; i < xCoordinates.length - 2; i++) {
             //point 1
-            double p1x = xCoordinates[i];
-            double p1y = yCoordinates[i];
+             p1x = xCoordinates[i];
+             p1y = yCoordinates[i];
 
             //point 2, always the vertex as per requirements
-            double p2x = xCoordinates[i + 1];
-            double p2y = yCoordinates[i + 1];
+             p2x = xCoordinates[i + 1];
+             p2y = yCoordinates[i + 1];
 
             //Point 3
-            double p3x = xCoordinates[i + 2];
-            double p3y = yCoordinates[i + 2];
+             p3x = xCoordinates[i + 2];
+             p3y = yCoordinates[i + 2];
 
             //if point 1 or 3 coincides with the vertex (p2) then skip!
             if (p1x == p2x && p1y == p2y || p3x == p2x && p3y == p2y)
                 continue;
 
-            double angle = Geometry.calculateAngle(p2x, p2y, p1x, p1y, p3x, p3y);
+            //amgle using (p2x,p2y) as the vertex
+            angle = Geometry.calculateAngle(p2x, p2y, p1x, p1y, p3x, p3y);
+            
             if (angle < (Math.PI - EPSILON) || angle > (Math.PI + EPSILON)) {
                 return true;
             }
